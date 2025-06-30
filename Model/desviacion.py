29e8d5e18def293edeb9698259de9fa41e3e7952
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-06-30 18:51:12 -0500

    Cambio a NumPy

--- Model/desviacion.py
+++ Model/desviacion.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-import statistics
+import numpy as np
 
 class Desviacion:
     def __init__(self, datos: list):
         self.datos = datos
-        self.media = statistics.mean(datos)
-        self.desviacion_estandar = statistics.stdev(datos)
+        self.media = float(np.mean(datos))
+        self.desviacion_estandar = float(np.std(datos, ddof=1))  
         self.cantidad = len(datos)
         self.fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
