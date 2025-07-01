ba9af06801093f267becbac441f2bc1bca3cea7c
Nicolás Arrieta <nicolas_aarrieta@soy.sena.edu.co>
2025-06-30 23:13:16 -0500

    Comentarios agg

--- Model/desviacion.py
+++ Model/desviacion.py
@@ -3,15 +3,15 @@ import numpy as np
 
 class Desviacion:
     def __init__(self, datos: list):
-        self.datos = datos
-        self.media = float(np.mean(datos))
-        self.desviacion_estandar = float(np.std(datos, ddof=1))  
-        self.cantidad = len(datos)
+        self.datos = datos      # Guarda la lista de números
+        self.media = float(np.mean(datos))      # Calcula la media y la guarda como float
+        self.desviacion_estandar = float(np.std(datos, ddof=1))        # Calcula y guarda la desviación estándar como float
+        self.cantidad = len(datos)      # Guarda la cantidad de números de la lista
         self.fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def to_insert_tuple(self):
         return (
-            ",".join(map(str, self.datos)),  
+            ",".join(map(str, self.datos)), # Convierte la lista de datos en un string
             self.media,
             self.desviacion_estandar,
             self.cantidad,
